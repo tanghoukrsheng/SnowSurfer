ec0a190c493bebc714fd24e6e5dd61444c0afde9
Sheng Tang <HSTAN1@Ulaval.ca>
2026-03-10 21:29:40 -0400

    add particles

--- Assets/Script/CrashDetector.cs
+++ Assets/Script/CrashDetector.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CrashDetector : MonoBehaviour
 {
+
+    [SerializeField] float delayBeforeReload = 2f;
+    [SerializeField] ParticleSystem crashParticle;
   void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor"); // find the index of the floor layer
         if (collision.gameObject.layer == layerIndex) // Check if the collided object is on the floor layer
         {
-            print("You lost!");
+           print("You lost!");
+           crashParticle.Play(); // Play the crash particle effect
+           Invoke("reloadScene", delayBeforeReload); // Call the reloadScene method after a delay of 2 seconds
         }
     }
+
+
+    void reloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reload the current scene to restart the game
+    }
+    
 }
--- Assets/Script/FinishLine.cs
+++ Assets/Script/FinishLine.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
+
+
 {
+    [SerializeField] float delayBeforeReload = 2f;
+    [SerializeField] ParticleSystem winParticle;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player"); // find the index of the player layer
         if (collision.gameObject.layer == layerIndex) // Check if the collided object is on the player layer
         {
             print("You win!");
+            winParticle.Play(); // Play the win particle effect
+            Invoke("reloadScene", delayBeforeReload); // Call the reloadScene method after a delay of 2 seconds
         }
+    }
+
+    void reloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reload the current scene to restart the game
     }
 }
