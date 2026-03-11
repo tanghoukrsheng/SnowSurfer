35329e680c92faaf9651dcb8f41c7e4fb2769889
Sheng Tang <HSTAN1@Ulaval.ca>
2026-03-10 23:57:46 -0400

    add snowTrail

--- Assets/Script/Player Controller.cs
+++ Assets/Script/Player Controller.cs
@@ -5,25 +5,46 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 1f;
-      InputAction moveAction;
-      Rigidbody2D rb;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
+    InputAction moveAction;
+    Rigidbody2D rb;
+    Vector2 moveVector;
+    SurfaceEffector2D surfaceEffector;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindObjectOfType<SurfaceEffector2D>(); // Find the SurfaceEffector2D in the scene
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector;
-        moveVector= moveAction.ReadValue<Vector2>();
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+
+    void RotatePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
 
         if (moveVector.x < 0)
         rb.AddTorque(torqueAmount); // Apply torque based on the horizontal input
         else if (moveVector.x > 0)
         rb.AddTorque(-torqueAmount); // Apply torque in the opposite direction for positive input
+    }
 
+    void BoostPlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        if (moveVector.y > 0)
+           surfaceEffector.speed = boostSpeed; // Set the speed of the SurfaceEffector2D to boostSpeed when the vertical input is positive
+        else
+           surfaceEffector.speed = baseSpeed; // Set the speed of the SurfaceEffector2
     }
 }
---
+++ Assets/Script/SnowTrail.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem TrailParticle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision detected!");
+        int layerIndex = LayerMask.NameToLayer("Floor"); // find the index of the floor layer
+        if (collision.gameObject.layer == layerIndex) // Check if the collided object is on the floor layer
+        {
+            print("Trail started!");
+            TrailParticle.Play(); // Play the trail particle effect
+
+            
+        }
+    }
+
+
+     void OnCollisionExit2D(Collision2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Floor"); // find the index of the floor layer
+        if (collision.gameObject.layer == layerIndex) // Check if the collided object is on the floor layer
+        {
+          
+             TrailParticle.Stop(); // Stop the trail particle effect when the player exits the floor collider
+            
+        }
+    }
+}
